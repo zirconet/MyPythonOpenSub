64000258c10d9287bb74da6a1525a3ede13798b2
Zirconet <cittadinoimperfetto@gmail.com>
2018-12-20 00:08:24 +0100

    Update mySubs.py

--- mySubs.py
+++ mySubs.py
@@ -19,13 +19,44 @@ class OpenSubtitles(object):
         return token
 
     def logout(self):
-        '''Returns True is logout is ok, otherwise None.
-        '''
         data = self.xmlrpc.LogOut(self.token)
         return '200' in data.get('status')
 
     def search_subtitles(self, params):
-        '''Returns a list with the subtitles info.
-        '''
         self.data = self.xmlrpc.SearchSubtitles(self.token, params)
         return self._get_from_data_or_none('data')
+
+        def __init__(self, path):
+        self.path = path
+        self.size = str(os.path.getsize(path))
+
+    def get_hash(self):
+        longlongformat = 'q'  # long long
+        bytesize = struct.calcsize(longlongformat)
+
+        try:
+            f = open(self.path, "rb")
+        except(IOError):
+            return "IOError"
+
+        hash = int(self.size)
+
+        if int(self.size) < 65536 * 2:
+            return "SizeError"
+
+        for x in range(65536 // bytesize):
+            buffer = f.read(bytesize)
+            (l_value, ) = struct.unpack(longlongformat, buffer)
+            hash += l_value
+            hash = hash & 0xFFFFFFFFFFFFFFFF  # to remain as 64bit number
+
+        f.seek(max(0, int(self.size) - 65536), 0)
+        for x in range(65536 // bytesize):
+            buffer = f.read(bytesize)
+            (l_value, ) = struct.unpack(longlongformat, buffer)
+            hash += l_value
+            hash = hash & 0xFFFFFFFFFFFFFFFF
+
+        f.close()
+        returnedhash = "%016x" % hash
+        return str(returnedhash)
